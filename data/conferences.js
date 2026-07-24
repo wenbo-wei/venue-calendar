52a9ac0adddf1426e12fadc07847021c60a28396
wenbo <wenbo.weiwb@gmail.com>
2026-07-24 16:06:50 +0800

    Initial release

---
+++ data/conferences.js
@@ -0,0 +1,440 @@
+window.AI_CONFERENCES = {
+  "updated_at": "2026-07-24T06:57:54.225926+00:00",
+  "upstream": "verified official conference websites",
+  "venues": [
+    {
+      "title": "AAAI",
+      "description": "AAAI Conference on Artificial Intelligence",
+      "rank": {
+        "ccf": "A"
+      },
+      "confs": [
+        {
+          "year": 2027,
+          "id": "aaai27",
+          "link": "https://aaai.org/conference/aaai/aaai-27/",
+          "link_kind": "edition",
+          "official_page_announced": true,
+          "timeline": [
+            {
+              "abstract_deadline": "2026-07-21 23:59:59",
+              "deadline": "2026-07-28 23:59:59"
+            }
+          ],
+          "timezone": "UTC-12",
+          "date": "TBD",
+          "place": "Montréal, Canada",
+          "place_status": "verified",
+          "location_source_url": "https://aaai.org/conference/aaai/aaai-27/"
+        }
+      ],
+      "latest_link": "https://aaai.org/conference/aaai/aaai-27/",
+      "official_url": "https://aaai.org/conference/aaai/aaai-27/",
+      "series_url": "https://aaai.org/conference/aaai/",
+      "next_year": 2027,
+      "source_category": "official",
+      "source_slug": "aaai"
+    },
+    {
+      "title": "ACM MM",
+      "description": "ACM International Conference on Multimedia",
+      "rank": {
+        "ccf": "A"
+      },
+      "confs": [
+        {
+          "year": 2027,
+          "id": "mm27",
+          "link": "https://www.acmmm.org/",
+          "link_kind": "series",
+          "official_page_announced": false,
+          "timeline": [],
+          "timezone": "UTC-12",
+          "date": "TBD",
+          "place": "Hong Kong",
+          "place_status": "verified",
+          "location_source_url": "https://www.acmmm.org/"
+        }
+      ],
+      "latest_link": "https://www.acmmm.org/",
+      "official_url": null,
+      "series_url": "https://www.acmmm.org/",
+      "next_year": 2027,
+      "source_category": "official",
+      "source_slug": "mm"
+    },
+    {
+      "title": "BMVC",
+      "description": "British Machine Vision Conference",
+      "rank": {
+        "ccf": "C"
+      },
+      "confs": [
+        {
+          "year": 2027,
+          "id": "bmvc27",
+          "link": "https://www.bmva.org/bmvc",
+          "link_kind": "series",
+          "official_page_announced": false,
+          "timeline": [],
+          "timezone": "UTC-12",
+          "date": "TBD",
+          "place": "TBD",
+          "place_status": "not_announced",
+          "location_source_url": null
+        }
+      ],
+      "latest_link": "https://www.bmva.org/bmvc",
+      "official_url": null,
+      "series_url": "https://www.bmva.org/bmvc",
+      "next_year": 2027,
+      "source_category": "official",
+      "source_slug": "bmvc"
+    },
+    {
+      "title": "CVPR",
+      "description": "IEEE/CVF Conference on Computer Vision and Pattern Recognition",
+      "rank": {
+        "ccf": "A"
+      },
+      "confs": [
+        {
+          "year": 2027,
+          "id": "cvpr27",
+          "link": "https://cvpr.thecvf.com/",
+          "link_kind": "series",
+          "official_page_announced": false,
+          "timeline": [],
+          "timezone": "UTC-12",
+          "date": "TBD",
+          "place": "TBD",
+          "place_status": "not_announced",
+          "location_source_url": null
+        }
+      ],
+      "latest_link": "https://cvpr.thecvf.com/",
+      "official_url": null,
+      "series_url": "https://cvpr.thecvf.com/",
+      "next_year": 2027,
+      "source_category": "official",
+      "source_slug": "cvpr"
+    },
+    {
+      "title": "ECAI",
+      "description": "European Conference on Artificial Intelligence",
+      "rank": {
+        "ccf": "B"
+      },
+      "confs": [
+        {
+          "year": 2027,
+          "id": "ecai27",
+          "link": "https://www.eurai.org/ecai",
+          "link_kind": "series",
+          "official_page_announced": false,
+          "timeline": [],
+          "timezone": "UTC-12",
+          "date": "TBD",
+          "place": "TBD",
+          "place_status": "not_announced",
+          "location_source_url": null
+        }
+      ],
+      "latest_link": "https://www.eurai.org/ecai",
+      "official_url": null,
+      "series_url": "https://www.eurai.org/ecai",
+      "next_year": 2027,
+      "source_category": "official",
+      "source_slug": "ecai"
+    },
+    {
+      "title": "ECCV",
+      "description": "European Conference on Computer Vision",
+      "rank": {
+        "ccf": "B"
+      },
+      "confs": [
+        {
+          "year": 2028,
+          "id": "eccv28",
+          "link": "https://eccv.ecva.net/",
+          "link_kind": "series",
+          "official_page_announced": false,
+          "timeline": [],
+          "timezone": "UTC-12",
+          "date": "TBD",
+          "place": "TBD",
+          "place_status": "not_announced",
+          "location_source_url": null
+        }
+      ],
+      "latest_link": "https://eccv.ecva.net/",
+      "official_url": null,
+      "series_url": "https://eccv.ecva.net/",
+      "next_year": 2028,
+      "source_category": "official",
+      "source_slug": "eccv"
+    },
+    {
+      "title": "ICASSP",
+      "description": "IEEE International Conference on Acoustics, Speech, and Signal Processing",
+      "rank": {
+        "ccf": "B"
+      },
+      "confs": [
+        {
+          "year": 2027,
+          "id": "icassp27",
+          "link": "https://2027.ieeeicassp.org/",
+          "link_kind": "edition",
+          "official_page_announced": true,
+          "timeline": [
+            {
+              "deadline": "2026-09-16 23:59:59"
+            }
+          ],
+          "timezone": "UTC-12",
+          "date": "TBD",
+          "place": "Toronto, Canada",
+          "place_status": "verified",
+          "location_source_url": "https://2027.ieeeicassp.org/"
+        }
+      ],
+      "latest_link": "https://2027.ieeeicassp.org/",
+      "official_url": "https://2027.ieeeicassp.org/",
+      "series_url": "https://signalprocessingsociety.org/events/conferences",
+      "next_year": 2027,
+      "source_category": "official",
+      "source_slug": "icassp"
+    },
+    {
+      "title": "ICCV",
+      "description": "IEEE/CVF International Conference on Computer Vision",
+      "rank": {
+        "ccf": "A"
+      },
+      "confs": [
+        {
+          "year": 2027,
+          "id": "iccv27",
+          "link": "https://iccv.thecvf.com/",
+          "link_kind": "series",
+          "official_page_announced": false,
+          "timeline": [],
+          "timezone": "UTC-12",
+          "date": "TBD",
+          "place": "TBD",
+          "place_status": "not_announced",
+          "location_source_url": null
+        }
+      ],
+      "latest_link": "https://iccv.thecvf.com/",
+      "official_url": null,
+      "series_url": "https://iccv.thecvf.com/",
+      "next_year": 2027,
+      "source_category": "official",
+      "source_slug": "iccv"
+    },
+    {
+      "title": "ICIP",
+      "description": "IEEE International Conference on Image Processing",
+      "rank": {
+        "ccf": "C"
+      },
+      "confs": [
+        {
+          "year": 2027,
+          "id": "icip27",
+          "link": "https://2027.ieeeicip.org/",
+          "link_kind": "edition",
+          "official_page_announced": true,
+          "timeline": [],
+          "timezone": "UTC-12",
+          "date": "TBD",
+          "place": "Singapore",
+          "place_status": "verified",
+          "location_source_url": "https://2027.ieeeicip.org/"
+        }
+      ],
+      "latest_link": "https://2027.ieeeicip.org/",
+      "official_url": "https://2027.ieeeicip.org/",
+      "series_url": "https://signalprocessingsociety.org/events/conferences",
+      "next_year": 2027,
+      "source_category": "official",
+      "source_slug": "icip"
+    },
+    {
+      "title": "ICLR",
+      "description": "International Conference on Learning Representations",
+      "rank": {
+        "ccf": "A"
+      },
+      "confs": [
+        {
+          "year": 2027,
+          "id": "iclr27",
+          "link": "https://iclr.cc/",
+          "link_kind": "series",
+          "official_page_announced": false,
+          "timeline": [],
+          "timezone": "UTC-12",
+          "date": "TBD",
+          "place": "West Coast North America",
+          "place_status": "verified",
+          "location_source_url": "https://iclr.cc/Conferences/FutureMeetings"
+        }
+      ],
+      "latest_link": "https://iclr.cc/",
+      "official_url": null,
+      "series_url": "https://iclr.cc/",
+      "next_year": 2027,
+      "source_category": "official",
+      "source_slug": "iclr"
+    },
+    {
+      "title": "ICME",
+      "description": "IEEE International Conference on Multimedia and Expo",
+      "rank": {
+        "ccf": "B"
+      },
+      "confs": [
+        {
+          "year": 2027,
+          "id": "icme27",
+          "link": "https://signalprocessingsociety.org/events/conferences",
+          "link_kind": "series",
+          "official_page_announced": false,
+          "timeline": [],
+          "timezone": "UTC-12",
+          "date": "TBD",
+          "place": "TBD",
+          "place_status": "not_announced",
+          "location_source_url": null
+        }
+      ],
+      "latest_link": "https://signalprocessingsociety.org/events/conferences",
+      "official_url": null,
+      "series_url": "https://signalprocessingsociety.org/events/conferences",
+      "next_year": 2027,
+      "source_category": "official",
+      "source_slug": "icme"
+    },
+    {
+      "title": "ICML",
+      "description": "International Conference on Machine Learning",
+      "rank": {
+        "ccf": "A"
+      },
+      "confs": [
+        {
+          "year": 2027,
+          "id": "icml27",
+          "link": "https://icml.cc/",
+          "link_kind": "series",
+          "official_page_announced": false,
+          "timeline": [],
+          "timezone": "UTC-12",
+          "date": "TBD",
+          "place": "South America",
+          "place_status": "verified",
+          "location_source_url": "https://icml.cc/Conferences/FutureMeetings"
+        }
+      ],
+      "latest_link": "https://icml.cc/",
+      "official_url": null,
+      "series_url": "https://icml.cc/",
+      "next_year": 2027,
+      "source_category": "official",
+      "source_slug": "icml"
+    },
+    {
+      "title": "IJCAI",
+      "description": "International Joint Conference on Artificial Intelligence",
+      "rank": {
+        "ccf": "B"
+      },
+      "confs": [
+        {
+          "year": 2027,
+          "id": "ijcai27",
+          "link": "https://www.ijcai.org/",
+          "link_kind": "series",
+          "official_page_announced": false,
+          "timeline": [],
+          "timezone": "UTC-12",
+          "date": "TBD",
+          "place": "TBD",
+          "place_status": "not_announced",
+          "location_source_url": null
+        }
+      ],
+      "latest_link": "https://www.ijcai.org/",
+      "official_url": null,
+      "series_url": "https://www.ijcai.org/",
+      "next_year": 2027,
+      "source_category": "official",
+      "source_slug": "ijcai"
+    },
+    {
+      "title": "NeurIPS",
+      "description": "Conference on Neural Information Processing Systems",
+      "rank": {
+        "ccf": "A"
+      },
+      "confs": [
+        {
+          "year": 2027,
+          "id": "nips27",
+          "link": "https://neurips.cc/",
+          "link_kind": "series",
+          "official_page_announced": false,
+          "timeline": [],
+          "timezone": "UTC-12",
+          "date": "TBD",
+          "place": "Europe",
+          "place_status": "verified",
+          "location_source_url": "https://neurips.cc/Conferences/FutureMeetings"
+        }
+      ],
+      "latest_link": "https://neurips.cc/",
+      "official_url": null,
+      "series_url": "https://neurips.cc/",
+      "next_year": 2027,
+      "source_category": "official",
+      "source_slug": "nips"
+    },
+    {
+      "title": "WACV",
+      "description": "IEEE/CVF Winter Conference on Applications of Computer Vision",
+      "rank": {
+        "ccf": "N"
+      },
+      "confs": [
+        {
+          "year": 2027,
+          "id": "wacv27",
+          "link": "https://wacv.thecvf.com/",
+          "link_kind": "edition",
+          "official_page_announced": true,
+          "timeline": [
+            {
+              "abstract_deadline": "2026-08-21 23:59:00",
+              "deadline": "2026-08-28 23:59:59"
+            }
+          ],
+          "timezone": "UTC-12",
+          "date": "TBD",
+          "place": "Disney Springs, Buena Vista, FL",
+          "place_status": "verified",
+          "location_source_url": "https://wacv.thecvf.com/"
+        }
+      ],
+      "latest_link": "https://wacv.thecvf.com/",
+      "official_url": "https://wacv.thecvf.com/",
+      "series_url": "https://wacv.thecvf.com/",
+      "next_year": 2027,
+      "source_category": "official",
+      "source_slug": "wacv"
+    }
+  ]
+};
